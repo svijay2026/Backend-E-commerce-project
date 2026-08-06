9360200c183863241f0ccfc3b72c33b9d3da6ee8
svijay2026 <surivijaykumar18@gmail.com>
2026-08-06 11:56:20 +0530

    Clean up mysql_table.py by removing comments and imports
    Removed comments and unused imports related to table creation.

--- mysql_table.py
+++ mysql_table.py
@@ -1,7 +1,1 @@
-# For creating table structure in database from python
 
-from database import Base,engine
-from models import Base
-import models
-
-Base.metadata.create_all(bind=engine)   # line used to create table structure using model module from python in mysql
