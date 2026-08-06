bb6524c0f4c5a83c4da8a69a4b060bd5fb6d8098
svijay2026 <surivijaykumar18@gmail.com>
2026-08-06 11:14:14 +0530

    Import models in mysql_table.py
    Import models module to ensure table creation works correctly.

--- mysql_table.py
+++ mysql_table.py
@@ -2,5 +2,6 @@
 
 from database import Base,engine
 from models import Base
+import models
 
 Base.metadata.create_all(bind=engine)   # line used to create table structure using model module from python in mysql
