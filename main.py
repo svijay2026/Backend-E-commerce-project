93c70400c740e83c77746bf99da9507a7ef04326
svijay2026 <surivijaykumar18@gmail.com>
2026-08-06 11:57:15 +0530

    Implement database table structure creation
    Added database table creation logic using SQLAlchemy models.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI                 # Import FastAPI to create the backend application instance.
 from routers import categories,products,user     # Import API routers to register different resource endpoints with the FastAPI application.
 
+# For creating table structure in database from python
+
+from database import Base,engine
+from models import Base
+import models
+
+Base.metadata.create_all(bind=engine)   # line used to create table structure using model module from python in mysql
+
+
 app=FastAPI()          # Create the main FastAPI application instance.
 
 app.include_router(categories.router)  # Register category routes with the main FastAPI application.
